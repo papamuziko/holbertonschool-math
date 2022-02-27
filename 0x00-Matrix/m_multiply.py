f6852cf9db67f18a1c5b1fa6ffe2216c1dca1ef3
Guillaume SALVA <ghinzu.iphone@gmail.com>
2022-02-26 20:36:10 -0800

    Create m_multiply.py

---
+++ 0x00-Matrix/m_multiply.py
@@ -0,0 +1,20 @@
+"""
+M_multiply module
+"""
+
+
+def mul(m1, m1):
+    """
+    multiple 2 matrices function
+    - m1
+    - m2
+    Return: m1 x m2
+    """
+    res = [[0 for x in len(m1)] for y in len(m2)]
+  
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                res[i][j] += m1[i][k] * m2[k][j]
+    
+    return res
