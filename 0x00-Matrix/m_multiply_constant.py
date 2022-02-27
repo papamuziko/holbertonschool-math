003ffd081cdec86c3604959d17656955251a68d3
Guillaume SALVA <ghinzu.iphone@gmail.com>
2022-02-26 20:16:11 -0800

    Update m_multiply_constant.py

--- 0x00-Matrix/m_multiply_constant.py
+++ 0x00-Matrix/m_multiply_constant.py
@@ -3,12 +3,12 @@ M_multiply_constant module
 """
 
 
-def mul_by(m1, i):
+def mul_by(m1, c):
     """
     multiple function by a constant
     - m1
-    - i
-    Return: m1 x i
+    - c
+    Return: m1 x c
     """
-    return [[m1[i][j] * i for j in range(len(m1[0]))] for i in
+    return [[m1[i][j] * c for j in range(len(m1[0]))] for i in
             range(len(m1))]
