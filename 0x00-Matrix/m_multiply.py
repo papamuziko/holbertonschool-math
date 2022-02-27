08d168572c03e583a4c42217a120d3464ae6ef03
Guillaume SALVA <ghinzu.iphone@gmail.com>
2022-02-26 20:51:57 -0800

    Update m_multiply.py

--- 0x00-Matrix/m_multiply.py
+++ 0x00-Matrix/m_multiply.py
@@ -11,10 +11,10 @@ def mul(m1, m2):
     Return: m1 x m2
     """
     res = [[0 for x in range(len(m2[0]))] for y in range(len(m1))]
-  
+
     for i in range(len(m1)):
         for j in range(len(m2[0])):
             for k in range(len(m2)):
                 res[i][j] += m1[i][k] * m2[k][j]
-    
+
     return res
