74349a326d751e1ed0ad0f9b0ac1bbe19523756d
Guillaume SALVA <ghinzu.iphone@gmail.com>
2022-02-27 20:01:10 -0800

    Update m_adding.py

--- 0x00-Matrix/m_adding.py
+++ 0x00-Matrix/m_adding.py
@@ -10,5 +10,5 @@ def add(m1, m2):
     - m2
     Return: m1 + m2
     """
-    return [[m1[i][j] + m2[i][j] for j in range(len(m1[0]))] for i in
+    return [[m1[i][j] - m2[i][j] for j in range(len(m1[0]))] for i in
             range(len(m1))]
